9b639b17543dba2961924221440ac666c79b304c
BABA <38986298+BABA983@users.noreply.github.com>
2025-04-09 10:52:22 +0800

    Change guicursor style

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,6 +9,7 @@
 -- vim.opt.pumblend = 5
 -- vim.opt.background = "dark"
 -- vim.opt.colorscheme = "darkplus"
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor20,t:block-blinkon500-blinkoff500-TermCursor"
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
